9e702736a701e1e8eb73d5ee06235808413b5a25
FannDGannn <finnegan0705@gmail.com>
2025-09-09 16:00:09 -0400

    Start Screen ver 2

---
+++ Scripts/button.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_start_button_pressed() -> void:
+	print("Start")
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	print("Credits")
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	print("Exit")
+	pass # Replace with function body.
