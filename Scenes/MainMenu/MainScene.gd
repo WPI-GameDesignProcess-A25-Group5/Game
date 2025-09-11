473aedef16c1e7657421ec513fa4738b62d175e9
ziahmad <zaahmad@wpi.edu>
2025-09-10 20:59:29 -0400

    fixed slpash screen and sound finishes while goin to correct place on button

--- Scenes/MainMenu/MainScene.gd
+++ Scenes/MainMenu/MainScene.gd
@@ -11,18 +11,29 @@ func _process(_delta: float) -> void:
 	pass
 
 
-
+var buttonPressed = ""
 func _on_start_button_pressed() -> void:
 	$Buttons/Click.play()
+	buttonPressed = "start"
+	
 
 func _on_credits_button_pressed() -> void:
 	$Buttons/Click.play()
+	buttonPressed = "credits"
 
 
 func _on_exit_button_pressed() -> void:
 	$Buttons/Click.play()
+	buttonPressed = "exit"
+	
 
 
 func _on_click_sound_finished() -> void:
-	get_tree().change_scene_to_packed(creditsScene)
+	match buttonPressed:
+		"start":
+			pass
+		"credits":
+			get_tree().change_scene_to_packed(creditsScene)
+		"exit":
+			get_tree().quit(0)
 	pass # Replace with function body.
