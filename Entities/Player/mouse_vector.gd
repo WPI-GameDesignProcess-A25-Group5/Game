ad46f0a5291048361604b7aab2e15fa5d37b19db
zaahmad <zaahmad@wpi.edu>
2025-09-13 17:27:14 -0400

    slingshot how only wokrs on button left and right, and doens't switch which one its looking for

--- Entities/Player/mouse_vector.gd
+++ Entities/Player/mouse_vector.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var mousePos2 :=$MousePosition2
 
 var direction:Vector2=Vector2.ZERO;
-var mouseDown = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,19 +12,21 @@ func _ready() -> void:
 signal slingShotFire(dir:Vector2)
 signal slingShotUpdate(dir:Vector2)
 
+var whichButtonPressed = MOUSE_BUTTON_NONE
 
 func _unhandled_input(event: InputEvent) -> void:
-	if(event is InputEventMouseButton and event.is_pressed()):
-		mouseDown = true
+	if(event is InputEventMouseButton and event.is_pressed() and (event.button_index == MOUSE_BUTTON_LEFT or event.button_index==MOUSE_BUTTON_RIGHT) ):
 		mousePos1.global_position = event.global_position-get_viewport_rect().size/2+get_viewport_rect().position
 		mousePos2.global_position = event.global_position-get_viewport_rect().size/2+get_viewport_rect().position
-	if(event is InputEventMouseMotion and mouseDown):
+		whichButtonPressed = event.button_index
+	if(event is InputEventMouseMotion and whichButtonPressed):
 		mousePos2.global_position += event.relative
 		direction = get_direction(mousePos1.global_position,mousePos2.global_position)
 		slingShotUpdate.emit(direction)
-	if(event is InputEventMouseButton and event.is_released()):
+	if(event is InputEventMouseButton and event.is_released() and event.button_index == whichButtonPressed):
+		whichButtonPressed=MOUSE_BUTTON_NONE
 		direction = get_direction(mousePos1.global_position,mousePos2.global_position)
-		mouseDown = false;
+
 		slingShotFire.emit(direction)
 		direction = Vector2.ZERO
 	
