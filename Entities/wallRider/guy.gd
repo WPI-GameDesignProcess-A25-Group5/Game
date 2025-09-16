466128fd03b7cbab86b95dc01b00ad9683bc61ab
zaahmad <zaahmad@wpi.edu>
2025-09-16 13:49:35 -0400

    setup

--- Entities/wallRider/guy.gd
+++ Entities/wallRider/guy.gd
@@ -1,48 +1,30 @@
 extends CharacterBody2D
 
+@export var SPEED = 300
 
-@export var SPEED:=300.0
-@export var MAX_FALL_SPEED :=1200.0
-
-
-var stickList :Array[BaseBlock]= []
-
-func _on_wall_rider_stick_to(dir: Vector2, body: BaseBlock) -> void:
-	#stickList.push_back(body)
-	#print("In, ",dir)
-	up_direction = dir
-	
-	pass # Replace with function body.
-
-
-func _on_wall_rider_unstick_from(body: BaseBlock) -> void:
-	stickList.erase(body)
-	#print("Gone, ",stickList)
-	
-	pass # Replace with function body.
+var left:bool = false
+var right:bool = false
+var leftCorner:bool = false
+var rightCorner:bool = false
 
 func _physics_process(delta: float) -> void:
-	$Sprite2D.rotation = -up_direction.angle_to(Vector2.UP)
-	
-	if(not is_on_floor()):
-		velocity += get_gravity().rotated(get_gravity().angle_to(-up_direction))*delta
-		if(velocity.dot(-up_direction)>MAX_FALL_SPEED):
-			velocity -= velocity.project(up_direction)+up_direction*MAX_FALL_SPEED
-	
-	else:	
-		var direction = Input.get_axis("MoveLeft","MoveRight")
-		if(direction):
-			var movedir = up_direction.rotated(direction*PI/2)
-			velocity += SPEED*movedir
-			var indir = velocity.project(movedir)
-			velocity -= indir - movedir*SPEED
-		else:
-			velocity.x = move_toward(velocity.x,0,SPEED)
-			velocity.y = move_toward(velocity.y,0,SPEED)
-	$WallRider.set_CheckVel(velocity)
-	#print(velocity)
-	#move_and_collide(velocity*delta)
-	move_and_slide()
-	
-	pass
-	
+	print(left," ",right," ",leftCorner," ",rightCorner)
+
+func _on_wall_rider_on_left(b: bool) -> void:
+	left =b
+	pass # Replace with function body.
+
+
+func _on_wall_rider_on_right(b: bool) -> void:
+	right =b
+	pass # Replace with function body.
+
+
+func _on_wall_rider_on_right_corner(b: bool) -> void:
+	rightCorner=b
+	pass # Replace with function body.
+
+
+func _on_wall_rider_onleft_corner(b: bool) -> void:
+	leftCorner=b
+	pass # Replace with function body.
--- Entities/wallRider/wall_rider.gd
+++ Entities/wallRider/wall_rider.gd
@@ -1,52 +1,78 @@
+#@tool
 extends StaticBody2D
 
+var boolList = {"Right":false,"Left":false,"Bottom":false,"BRight":false,"BLeft":false}
 
-signal stickTo(dir:Vector2, body:BaseBlock)
-signal unstickFrom(body:BaseBlock)
-
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if(body is BaseBlock):
-		if(body.stickable):
-			var dir = global_position - body.global_position
-			stickTo.emit(dir,body)
-	pass # Replace with function body.
-	
-	
+signal onLeft (b:bool)
+signal onRight (b:bool)
+signal onleftCorner(b:bool)
+signal onRightCorner(b:bool)
 
 
-func _on_body_exited(body: Node2D) -> void:
-	unstickFrom.emit(body)
-	pass # Replace with function body.
-
-var velCheck:Vector2=Vector2.ZERO
-
-func set_CheckVel(vel:Vector2):
-	velCheck = vel
-	
-var current:BaseBlock=null
-var colliding = false
-var posi
-
-func _physics_process(delta: float) -> void:
-	var collisions = move_and_collide(velCheck*delta,true)
-	#print(collisions)
-	if(collisions and collisions.get_collider() is BaseBlock):
-		var body = collisions.get_collider()
-		if(body.stickable):
-			posi = collisions.get_position()
-			current = body
-			#collisions.get_
-			var dir = collisions.get_normal()
-			stickTo.emit(dir,body)
-			colliding = true
-	else:
-		colliding = false
-			#print(dir)
-	if(current):
-		$RayCast2D.target_position =posi - $RayCast2D.global_position
-		var normal = $RayCast2D.get_collision_normal()
-		#var tangent = Vector2(-normal.y, normal.x) # perpendicular
-		stickTo.emit(normal,current)
+func _ready() -> void:
+	pass
 		
+func _process(delta: float) -> void:
+		onLeft.emit(boolList["Left"])
+		onRight.emit(boolList["Right"])
+		onleftCorner.emit(boolList["Bottom"] and boolList["BRight"] and not boolList["BLeft"])
+		onRightCorner.emit(boolList["Bottom"] and not boolList["BRight"] and boolList["BLeft"])
+	
+	
+		pass
+		#print(children)
+		
+
+
+func _on_right_body_entered(body: Node2D) -> void:
+	boolList["Right"]= true
+	pass # Replace with function body.
+
+
+func _on_right_body_exited(body: Node2D) -> void:
+	boolList["Right"]= false
+	pass # Replace with function body.
+
+
+func _on_left_body_entered(body: Node2D) -> void:
+	boolList["Left"] = true
+	pass # Replace with function body.
+
+
+func _on_left_body_exited(body: Node2D) -> void:
+	boolList["Left"] = false
+	
+	pass # Replace with function body.
+
+
+func _on_bl_body_entered(body: Node2D) -> void:
+	boolList["BLeft"] = true
+	
+	pass # Replace with function body.
+
+
+func _on_bl_body_exited(body: Node2D) -> void:
+	boolList["BLeft"] = false
+	
+	pass # Replace with function body.
+
+
+func _on_bot_body_entered(body: Node2D) -> void:
+	boolList["Bottom"] = true
+	pass # Replace with function body.
+
+
+func _on_bot_body_exited(body: Node2D) -> void:
+	boolList["Bottom"] = false
+	
+	pass # Replace with function body.
+
+
+func _on_br_body_entered(body: Node2D) -> void:
+	boolList["BRight"] = true
+	pass # Replace with function body.
+
+
+func _on_br_body_exited(body: Node2D) -> void:
+	boolList["BRight"] = false
+	pass # Replace with function body.
