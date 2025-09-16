dfae3d9fc26cc54aa7e731b75f5171a4b41743a1
zaahmad <zaahmad@wpi.edu>
2025-09-16 16:36:25 -0400

    kind of works. is a bit snappy and no longer works on round things - idea might be to try to get curavature (raycast down and get slope) and turn by that amount and tween

--- Entities/wallRider/guy.gd
+++ Entities/wallRider/guy.gd
@@ -2,29 +2,60 @@ extends CharacterBody2D
 
 @export var SPEED = 300
 
-var left:bool = false
-var right:bool = false
-var leftCorner:bool = false
-var rightCorner:bool = false
+var markerPos:Vector2
 
+func _ready():
+	markerPos = $Pivot.position
+
+var stuck = false
+var actionable =false
 func _physics_process(delta: float) -> void:
-	print(left," ",right," ",leftCorner," ",rightCorner)
-
-func _on_wall_rider_on_left(b: bool) -> void:
-	left =b
-	pass # Replace with function body.
-
-
-func _on_wall_rider_on_right(b: bool) -> void:
-	right =b
-	pass # Replace with function body.
-
-
-func _on_wall_rider_on_right_corner(b: bool) -> void:
-	rightCorner=b
-	pass # Replace with function body.
-
-
-func _on_wall_rider_onleft_corner(b: bool) -> void:
-	leftCorner=b
-	pass # Replace with function body.
+	
+	#$RayCast2D.target_position = up_direction*20
+	#print($WallRider.isOnLeft()," ",$WallRider.isOnRight(), " ", $WallRider.isOnleftCorner(), " ", $WallRider.isOnRightCorner())
+		
+		
+	#if false:
+	if not stuck:
+		velocity += get_gravity()*delta
+	
+	if(actionable):
+		if($WallRider.isOnleftCorner()):
+			print("LC")
+			up_direction =  up_direction.rotated(-PI/2)
+			global_position -= ($CollisionShape2D.shape.get_rect().size/2).rotated(-up_direction.angle_to(Vector2.UP))
+			global_position-=up_direction*SPEED*delta
+			#velocity = Vector2.ZERO
+			rotate(-PI/2)
+		if($WallRider.isOnRightCorner()):
+			print("RC")
+			up_direction =  up_direction.rotated(PI/2)
+			global_position += ($CollisionShape2D.shape.get_rect().size/2).rotated(-up_direction.angle_to(Vector2.UP)-PI/2)
+			global_position-=up_direction*SPEED*delta
+			#velocity = Vector2.ZERO
+			rotate(PI/2)
+		if($WallRider.isOnRight()):
+			print("RIGHT")
+			up_direction =  up_direction.rotated(-PI/2)
+			rotate(-PI/2)
+		if($WallRider.isOnLeft()):
+			print("LEFT")
+			up_direction = up_direction.rotated(PI/2)
+			rotate(PI/2)
+		var direction = Input.get_axis("MoveLeft","MoveRight")
+		if(direction):
+			velocity = SPEED*up_direction.rotated(direction*PI/2)
+		else:
+			#pass
+			velocity.x = move_toward(velocity.x,0,SPEED)
+			velocity.y = move_toward(velocity.y,0,SPEED)
+		
+	#print(up_direction)
+		
+	#move_and_slide()
+	var collision = move_and_collide(velocity*delta)
+	
+	if(collision):
+		actionable = true
+		#velocity -=collision.
+	
--- Entities/wallRider/wall_rider.gd
+++ Entities/wallRider/wall_rider.gd
@@ -1,78 +1,102 @@
 #@tool
-extends StaticBody2D
+extends Node2D
 
 var boolList = {"Right":false,"Left":false,"Bottom":false,"BRight":false,"BLeft":false}
-
-signal onLeft (b:bool)
-signal onRight (b:bool)
-signal onleftCorner(b:bool)
-signal onRightCorner(b:bool)
-
 
 func _ready() -> void:
 	pass
 		
-func _process(delta: float) -> void:
-		onLeft.emit(boolList["Left"])
-		onRight.emit(boolList["Right"])
-		onleftCorner.emit(boolList["Bottom"] and boolList["BRight"] and not boolList["BLeft"])
-		onRightCorner.emit(boolList["Bottom"] and not boolList["BRight"] and boolList["BLeft"])
-	
-	
+func _physics_process(delta: float) -> void:
+		#print(boolList)
 		pass
 		#print(children)
+
+func isOnLeft():
+	return boolList["Left"]
+func isOnRight():
+	return boolList["Right"]
+func isOnleftCorner():
+	return not boolList["Bottom"] and boolList["BRight"] and not boolList["BLeft"] and not boolList["Left"] and not boolList["Right"]
+func isOnRightCorner():
+	return not boolList["Bottom"] and not boolList["BRight"] and boolList["BLeft"] and not boolList["Right"] and not boolList["Left"]
 		
 
 
+var rbo
 func _on_right_body_entered(body: Node2D) -> void:
+	rbo = body
 	boolList["Right"]= true
+	$Right.modulate = Color.RED
 	pass # Replace with function body.
 
 
 func _on_right_body_exited(body: Node2D) -> void:
-	boolList["Right"]= false
+	if(body==rbo):
+		boolList["Right"]= false
+		$Right.modulate = Color.WHITE
+	
 	pass # Replace with function body.
 
-
+var lbo
 func _on_left_body_entered(body: Node2D) -> void:
+	lbo = body
 	boolList["Left"] = true
-	pass # Replace with function body.
-
-
-func _on_left_body_exited(body: Node2D) -> void:
-	boolList["Left"] = false
+	$Left.modulate = Color.RED
 	
 	pass # Replace with function body.
 
 
+func _on_left_body_exited(body: Node2D) -> void:
+	if(lbo==body):
+		boolList["Left"] = false
+		$Left.modulate = Color.WHITE
+	
+	pass # Replace with function body.
+
+var lbbo
 func _on_bl_body_entered(body: Node2D) -> void:
+	lbbo = body
 	boolList["BLeft"] = true
+	$BL.modulate = Color.RED
 	
 	pass # Replace with function body.
 
 
 func _on_bl_body_exited(body: Node2D) -> void:
-	boolList["BLeft"] = false
+	if lbbo == body:
+		boolList["BLeft"] = false
+		$BL.modulate = Color.WHITE
 	
 	pass # Replace with function body.
 
-
+var bbo
 func _on_bot_body_entered(body: Node2D) -> void:
+	bbo= body
 	boolList["Bottom"] = true
+	$Bot.modulate = Color.RED
+	
 	pass # Replace with function body.
 
 
 func _on_bot_body_exited(body: Node2D) -> void:
-	boolList["Bottom"] = false
+	if bbo==body:
+		boolList["Bottom"] = false
+		$Bot.modulate = Color.WHITE
+	
+	pass # Replace with function body.
+
+var rbbo
+func _on_br_body_entered(body: Node2D) -> void:
+	rbbo=body
+	boolList["BRight"] = true
+	$BR.modulate = Color.RED
 	
 	pass # Replace with function body.
 
 
-func _on_br_body_entered(body: Node2D) -> void:
-	boolList["BRight"] = true
-	pass # Replace with function body.
-
-
 func _on_br_body_exited(body: Node2D) -> void:
-	boolList["BRight"] = false
+	if rbbo==body:
+		boolList["BRight"] = false
+		$BR.modulate = Color.WHITE
+	
 	pass # Replace with function body.
