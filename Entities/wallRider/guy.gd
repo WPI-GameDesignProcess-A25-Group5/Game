ff9ec2a19dbfe9b5d1cb0400e6ad5e14e0b789d3
zaahmad <zaahmad@wpi.edu>
2025-09-17 19:05:06 -0400

    removed some unsied thigns in walrider folder and guy no longer rapidly switches if between a C shaped crevice

--- Entities/wallRider/guy.gd
+++ Entities/wallRider/guy.gd
@@ -2,16 +2,14 @@ extends CharacterBody2D
 
 @export var SPEED = 300
 
-var markerPos:Vector2
 
 func _ready():
-	markerPos = $Pivot.position
+	pass
 
 var stuck = false
 var actionable =false
 func _physics_process(delta: float) -> void:
 	
-	#$RayCast2D.target_position = up_direction*20
 	#print($WallRider.isOnLeft()," ",$WallRider.isOnRight(), " ", $WallRider.isOnleftCorner(), " ", $WallRider.isOnRightCorner())
 		
 		
@@ -20,30 +18,31 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity()*delta
 	
 	if(actionable):
-		if($WallRider.isOnleftCorner()):
-			print("LC")
-			up_direction =  up_direction.rotated(-PI/2)
-			global_position -= ($CollisionShape2D.shape.get_rect().size/2).rotated(-up_direction.angle_to(Vector2.UP))
-			global_position-=up_direction*SPEED*delta
-			#velocity = Vector2.ZERO
-			rotate(-PI/2)
-		if($WallRider.isOnRightCorner()):
-			print("RC")
-			up_direction =  up_direction.rotated(PI/2)
-			global_position += ($CollisionShape2D.shape.get_rect().size/2).rotated(-up_direction.angle_to(Vector2.UP)-PI/2)
-			global_position-=up_direction*SPEED*delta
-			#velocity = Vector2.ZERO
-			rotate(PI/2)
-		if($WallRider.isOnRight()):
-			print("RIGHT")
-			up_direction =  up_direction.rotated(-PI/2)
-			rotate(-PI/2)
-		if($WallRider.isOnLeft()):
-			print("LEFT")
-			up_direction = up_direction.rotated(PI/2)
-			rotate(PI/2)
+
 		var direction = Input.get_axis("MoveLeft","MoveRight")
 		if(direction):
+			if($WallRider.isOnleftCorner()):
+				print("LC")
+				up_direction =  up_direction.rotated(-PI/2)
+				global_position -= ($CollisionShape2D.shape.get_rect().size/2).rotated(-up_direction.angle_to(Vector2.UP))
+				global_position-=up_direction*SPEED*delta
+				#velocity = Vector2.ZERO
+				rotate(-PI/2)
+			if($WallRider.isOnRightCorner()):
+				print("RC")
+				up_direction =  up_direction.rotated(PI/2)
+				global_position += ($CollisionShape2D.shape.get_rect().size/2).rotated(-up_direction.angle_to(Vector2.UP)-PI/2)
+				global_position-=up_direction*SPEED*delta
+				#velocity = Vector2.ZERO
+				rotate(PI/2)
+			if($WallRider.isOnRight()):
+				print("RIGHT")
+				up_direction =  up_direction.rotated(-PI/2)
+				rotate(-PI/2)
+			if($WallRider.isOnLeft()):
+				print("LEFT")
+				up_direction = up_direction.rotated(PI/2)
+				rotate(PI/2)
 			velocity = SPEED*up_direction.rotated(direction*PI/2)
 		else:
 			#pass
