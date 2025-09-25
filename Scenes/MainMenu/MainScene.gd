63c0e07ca5095e10d8c9c28485311520cf8d70de
JackMorris1234 <jvmorris@wpi.edu>
2025-09-25 00:35:17 -0400

    Updated version notes and integrated new map
    Added 0.3 for version notes and integrated the new map tileset. Changed player stick detection to include tilesets until new solution found.

--- Scenes/MainMenu/MainScene.gd
+++ Scenes/MainMenu/MainScene.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var creditsScene = preload("res://Scenes/Credits/Credits.tscn")
 var testSence = preload("res://Scenes/TestScene/test_scene.tscn")
-
+var example_map=preload("res://Scenes/Map.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,7 +29,9 @@ func _on_exit_button_pressed() -> void:
 	$Buttons/Click.play()
 	buttonPressed = "exit"
 	
-
+func _on_example_pressed() -> void:
+	$Buttons/Click.play()
+	buttonPressed = "example"
 
 func _on_click_sound_finished() -> void:
 	match buttonPressed:
@@ -38,6 +40,8 @@ func _on_click_sound_finished() -> void:
 			pass
 		"credits":
 			get_tree().change_scene_to_packed(creditsScene)
+		"example":
+			get_tree().change_scene_to_packed(example_map)
 		"exit":
 			get_tree().quit(0)
 	pass # Replace with function body.
