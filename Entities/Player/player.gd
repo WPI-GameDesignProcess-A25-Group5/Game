62c1d614bba8d513f951b736fd7f30eaa811c690
IdiotEclipse <110128338+IdiotEclipse@users.noreply.github.com>
2025-10-07 11:29:57 -0400

    Hit flash
    Hit flash animation. It only works for the godot place holder enemies for some reason

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -7,7 +7,8 @@ const MAX_FALL_SPEED=1200
 
 @export var MAX_HEALTH := 3
 
-
+@onready var hitflash_ani: AnimationPlayer = $hitflash_ani
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 var walldirections := {}
@@ -47,11 +48,15 @@ func _ready() -> void:
 
 func _on_damaged(amount, _current_health, bywho):
 	hit(amount,bywho)
+	hitflash_ani.play("hitflash")
+	
 	
 func _on_death(_amount, bywho):
 	print("Died from", bywho.name)
 	Engine.time_scale=0.5
 	death(_amount, bywho)
+	animated_sprite_2d.play("Death")
+	
 
 
 func _on_timer_timeout() -> void:
