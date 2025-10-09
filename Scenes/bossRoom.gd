1593d7f3197c8f79d4388de2a579e880991867c7
zaahmad <zaahmad@wpi.edu>
2025-10-08 23:50:13 -0400

    made things easier to read

--- Scenes/bossRoom.gd
+++ Scenes/bossRoom.gd
@@ -16,7 +16,7 @@ func _exit_tree() -> void:
 	PauseScreen.allowPausing = false	
 
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	Transition.startTransition()
 	var temp = func(tree):
 		tree.change_scene_to_file("res://Scenes/MainMenu/start_screen.tscn")
